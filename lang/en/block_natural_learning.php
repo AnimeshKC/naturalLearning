356aaba1d36916023418d0ccba9f69c04b7d8757
Animesh KC <32381905+AnimeshKC@users.noreply.github.com>
2019-05-28 09:34:24 -0600

    Add files via upload

---
+++ lang/en/block_natural_learning.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+ *  strings for naturalLearning
+ *
+ * @package    natural_learning
+ * @copyright  2018 Animesh KC <animesh@ualberta.ca>
+ 
+ */
+
+$string['natural_learning'] = 'Natural Learning';
+$string['showCategories'] = 'displays a courses category';
+$string['block_natural_learning'] = 'natural Learning block';
+$string['pluginname'] = 'natural_learning';
+$string['blockString'] = 'Block string is the string inside our block.';
+$string['generate'] = 'Generate the page without updating';
+$string['update'] = 'Update the page';
